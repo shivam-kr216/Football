d63dde466f1883851fb1a521906aee7c9eb54333
shivam-kr216 <shivamkr216.com>
2021-01-20 11:20:59 +0530

    first commit

---
+++ src/components/IncomingMatches.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class IncomingMatches extends Component {
+    render() {
+        return (
+            <div className="main-container">
+                
+            </div>
+        )
+    }
+}
+
+export default IncomingMatches
